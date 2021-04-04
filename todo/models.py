cf134ec39ab376f5401d01a8d4e950801933f366
sergio <sergio.sousa@eezyr.com>
2021-04-04 15:55:38 +0200

    Tutorial 3 - Django Admin

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Todo(models.Model):
 
     title = models.CharField(max_length=255)
-    due_data = models.DateField()
+    due_date = models.DateField()
     completed = models.BooleanField()
     favourite = models.BooleanField()
 
@@ -14,3 +14,7 @@ class Todo(models.Model):
 class TodoList(models.Model):
 
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Todo List'
+        verbose_name_plural = 'Todo Lists'
