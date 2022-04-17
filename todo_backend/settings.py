1fb3b14c8938a3a4859a166f2afd97b2d8e04309
Simple Tech <simpletechprod@gmail.com>
2022-04-17 06:57:13 +0200

    Before upload video

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = '$+@n_x3l=m1w-y@k6vz0xt4+(!g^obms!ieusx848gtkjel7tf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
